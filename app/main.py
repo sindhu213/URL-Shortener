9fa054927a46bb075dd64f7b6a075c48260e7586
Sindhu <ksindhu571@gmail.com>
2024-03-09 09:32:51 +0530

    Updated project structure

--- app/main.py
+++ app/main.py
@@ -1,35 +1,15 @@
 from flask import Flask, render_template, redirect, request, send_file
 from utilities import generate_short_url
-from flask_sqlalchemy import SQLAlchemy
 import pandas as pd
 from io import BytesIO
+from models import db, URLMapping, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///site.db"
 
-db = SQLAlchemy(app)
+db.init_app(app)
 
-# models
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(40), unique=True, nullable=False)
-    history = db.relationship('URLMapping',backref="user", lazy=True)
-
-    def __repr__(self) -> str:
-        return f"Username : {self.username}, Email: {self.email}"
-
-class URLMapping(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    long_url = db.Column(db.String(100), nullable=False)
-    shortened_url = db.Column(db.String(100),nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-    def __repr__(self) -> str:
-        return f"Long URL: {self.long_url}, Shortened URL: {self.shortened_url}"
-
-
-#services
+#Routes
 @app.route('/', methods=["GET", "POST"])
 def index():
     if request.method == "POST":
@@ -55,10 +35,12 @@ def redirect_to_url(short_url):
     else:
         return render_template("404.html"), 404
 
+
 @app.route('/mapping')
 def mapping():
     data = URLMapping.query.all()
     return render_template("mapping.html", data=data)
+
 
 @app.route('/download')
 def download_mapping():
@@ -69,6 +51,7 @@ def download_mapping():
     df.to_excel(excel_file, index=False, sheet_name="URLMapping")
     excel_file.seek(0)
     return send_file(excel_file, as_attachment=True, download_name='url_mapping.xlsx')
+
 
 if __name__ == "__main__":  
     with app.app_context():
---
+++ app/models.py
@@ -0,0 +1,21 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(40), unique=True, nullable=False)
+    history = db.relationship('URLMapping',backref="user", lazy=True)
+
+    def __repr__(self) -> str:
+        return f"Username : {self.username}, Email: {self.email}"
+
+class URLMapping(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    long_url = db.Column(db.String(100), nullable=False)
+    shortened_url = db.Column(db.String(100),nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self) -> str:
+        return f"Long URL: {self.long_url}, Shortened URL: {self.shortened_url}"
